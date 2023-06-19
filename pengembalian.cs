59646a5e9796a636756c3a9e755129e7f56b80b9
septianii30 <134833925+septianii30@users.noreply.github.com>
2023-06-19 14:18:02 +0700

    Create pengembalian.cs

---
+++ pengembalian.cs
@@ -0,0 +1,313 @@
+namespace aplikasidekstopperpustakaan
+{
+    partial class Pengembalian
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label = new System.Windows.Forms.Label();
+            this.btn_kembali = new System.Windows.Forms.Button();
+            this.numeric = new System.Windows.Forms.NumericUpDown();
+            this.textBox_pengarang = new System.Windows.Forms.TextBox();
+            this.textBox_namaanggota = new System.Windows.Forms.TextBox();
+            this.textBox_idanggota = new System.Windows.Forms.TextBox();
+            this.textBox_id = new System.Windows.Forms.TextBox();
+            this.textBox_kode = new System.Windows.Forms.TextBox();
+            this.textBox_judul = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_terlambat = new System.Windows.Forms.TextBox();
+            this.textBox_hilang = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label10 = new System.Windows.Forms.Label();
+            this.date = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numeric)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Sitka Heading", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label.Location = new System.Drawing.Point(343, 9);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(131, 28);
+            this.label.TabIndex = 1;
+            this.label.Text = "Pengembalian";
+            // 
+            // btn_kembali
+            // 
+            this.btn_kembali.Location = new System.Drawing.Point(644, 338);
+            this.btn_kembali.Name = "btn_kembali";
+            this.btn_kembali.Size = new System.Drawing.Size(103, 32);
+            this.btn_kembali.TabIndex = 36;
+            this.btn_kembali.Text = "Pengembalian";
+            this.btn_kembali.UseVisualStyleBackColor = true;
+            this.btn_kembali.Click += new System.EventHandler(this.btn_kembali_Click);
+            // 
+            // numeric
+            // 
+            this.numeric.Location = new System.Drawing.Point(136, 366);
+            this.numeric.Name = "numeric";
+            this.numeric.Size = new System.Drawing.Size(130, 20);
+            this.numeric.TabIndex = 35;
+            // 
+            // textBox_pengarang
+            // 
+            this.textBox_pengarang.Location = new System.Drawing.Point(451, 338);
+            this.textBox_pengarang.Name = "textBox_pengarang";
+            this.textBox_pengarang.Size = new System.Drawing.Size(130, 20);
+            this.textBox_pengarang.TabIndex = 33;
+            // 
+            // textBox_namaanggota
+            // 
+            this.textBox_namaanggota.Location = new System.Drawing.Point(451, 392);
+            this.textBox_namaanggota.Name = "textBox_namaanggota";
+            this.textBox_namaanggota.Size = new System.Drawing.Size(130, 20);
+            this.textBox_namaanggota.TabIndex = 32;
+            // 
+            // textBox_idanggota
+            // 
+            this.textBox_idanggota.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox_idanggota.Location = new System.Drawing.Point(451, 366);
+            this.textBox_idanggota.Name = "textBox_idanggota";
+            this.textBox_idanggota.Size = new System.Drawing.Size(130, 20);
+            this.textBox_idanggota.TabIndex = 31;
+            // 
+            // textBox_id
+            // 
+            this.textBox_id.Location = new System.Drawing.Point(136, 312);
+            this.textBox_id.Name = "textBox_id";
+            this.textBox_id.Size = new System.Drawing.Size(130, 20);
+            this.textBox_id.TabIndex = 30;
+            // 
+            // textBox_kode
+            // 
+            this.textBox_kode.Location = new System.Drawing.Point(136, 394);
+            this.textBox_kode.Name = "textBox_kode";
+            this.textBox_kode.Size = new System.Drawing.Size(130, 20);
+            this.textBox_kode.TabIndex = 29;
+            // 
+            // textBox_judul
+            // 
+            this.textBox_judul.Location = new System.Drawing.Point(451, 308);
+            this.textBox_judul.Name = "textBox_judul";
+            this.textBox_judul.Size = new System.Drawing.Size(130, 20);
+            this.textBox_judul.TabIndex = 28;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(322, 399);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(78, 13);
+            this.label9.TabIndex = 27;
+            this.label9.Text = "Nama Anggota";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(322, 373);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(59, 13);
+            this.label8.TabIndex = 26;
+            this.label8.Text = "Id Anggota";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(322, 345);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(87, 13);
+            this.label7.TabIndex = 25;
+            this.label7.Text = "Pengarang Buku";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(322, 315);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 13);
+            this.label6.TabIndex = 24;
+            this.label6.Text = "Judul Buku";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 401);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 13);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "Kode Buku";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 373);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 13);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "Total Pinjam";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 345);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 13);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Tanggal Peminjaman";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 317);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Id Peminjaman";
+            // 
+            // textBox_terlambat
+            // 
+            this.textBox_terlambat.Location = new System.Drawing.Point(427, 448);
+            this.textBox_terlambat.Name = "textBox_terlambat";
+            this.textBox_terlambat.Size = new System.Drawing.Size(130, 20);
+            this.textBox_terlambat.TabIndex = 37;
+            // 
+            // textBox_hilang
+            // 
+            this.textBox_hilang.Location = new System.Drawing.Point(151, 448);
+            this.textBox_hilang.Name = "textBox_hilang";
+            this.textBox_hilang.Size = new System.Drawing.Size(130, 20);
+            this.textBox_hilang.TabIndex = 38;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(349, 451);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 39;
+            this.label1.Text = "Keterlambatan";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(71, 40);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(656, 237);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(81, 453);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(60, 13);
+            this.label10.TabIndex = 40;
+            this.label10.Text = "Kehilangan";
+            // 
+            // date
+            // 
+            this.date.Location = new System.Drawing.Point(136, 338);
+            this.date.Name = "date";
+            this.date.Size = new System.Drawing.Size(130, 20);
+            this.date.TabIndex = 41;
+            // 
+            // Pengembalian
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(795, 519);
+            this.Controls.Add(this.date);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_hilang);
+            this.Controls.Add(this.textBox_terlambat);
+            this.Controls.Add(this.btn_kembali);
+            this.Controls.Add(this.numeric);
+            this.Controls.Add(this.textBox_pengarang);
+            this.Controls.Add(this.textBox_namaanggota);
+            this.Controls.Add(this.textBox_idanggota);
+            this.Controls.Add(this.textBox_id);
+            this.Controls.Add(this.textBox_kode);
+            this.Controls.Add(this.textBox_judul);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Pengembalian";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pengembalian";
+            this.Load += new System.EventHandler(this.Pengembalian_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numeric)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Button btn_kembali;
+        private System.Windows.Forms.NumericUpDown numeric;
+        private System.Windows.Forms.TextBox textBox_pengarang;
+        private System.Windows.Forms.TextBox textBox_namaanggota;
+        private System.Windows.Forms.TextBox textBox_idanggota;
+        private System.Windows.Forms.TextBox textBox_id;
+        private System.Windows.Forms.TextBox textBox_kode;
+        private System.Windows.Forms.TextBox textBox_judul;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_terlambat;
+        private System.Windows.Forms.TextBox textBox_hilang;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox date;
+    }
+}
